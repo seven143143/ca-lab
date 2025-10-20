c9093e80955d03b35248ff10da837c50268746fb
bBrozedd <brooklynzhou249@gmail.com>
2025-10-20 18:16:55 -0400

    Remove TODO comment in LogoutInteractor
    Removed TODO comment for implementing Logout Use Case logic.

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -18,7 +18,6 @@ public class LogoutInteractor implements LogoutInputBoundary {
 
     @Override
     public void execute() {
-        // TODO: implement the logic of the Logout Use Case
         // * set the current username to null in the DAO
         // * instantiate the `LogoutOutputData`, which needs to contain the username.
         // * tell the presenter to prepare a success view.
