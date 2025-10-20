68268a56dcc0deafb8646fe62a5eacb27b46a5e0
Olivia Zhou <oliviajq.zhou@mail.utoronto.ca>
2025-10-20 18:09:48 -0400

    logoutController finished

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -1,6 +1,8 @@
 package interface_adapter.logout;
 
+import use_case.login.LoginInputData;
 import use_case.logout.LogoutInputBoundary;
+import use_case.logout.LogoutOutputData;
 
 /**
  * The controller for the Logout Use Case.
@@ -10,13 +12,15 @@ public class LogoutController {
     private LogoutInputBoundary logoutUseCaseInteractor;
 
     public LogoutController(LogoutInputBoundary logoutUseCaseInteractor) {
-        // TODO: Save the interactor in the instance variable.
+        this.logoutUseCaseInteractor = logoutUseCaseInteractor;
     }
 
     /**
      * Executes the Logout Use Case.
      */
     public void execute() {
-        // TODO: run the use case interactor for the logout use case
+
+        logoutUseCaseInteractor.execute();
+
     }
 }
